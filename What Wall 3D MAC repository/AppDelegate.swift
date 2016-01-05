69d33001faddc744694cffeb60e178971e4f3573
Edmond <George@edmonds-macbook-pro.local>
2016-01-05 11:53:08 +0900

    Initial Commit

---
+++ What Wall 3D MAC repository/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  What Wall 3D MAC repository
+//
+//  Created by Edmond on 1/5/16.
+//  Copyright (c) 2016 Future. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
